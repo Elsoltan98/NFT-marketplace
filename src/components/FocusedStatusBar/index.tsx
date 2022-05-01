4464bbec8129221f89d734fb78a13dace9e42678
MACBOOK <ben10a2992@gmail.com>
2022-05-01 11:54:11 +0200

    Add custom statusbar

---
+++ src/components/FocusedStatusBar/index.tsx
@@ -0,0 +1,15 @@
+import {StatusBar, StatusBarProps} from 'react-native';
+import React from 'react';
+import {useIsFocused} from '@react-navigation/native';
+
+const FocusedStatusBar = (
+  props: JSX.IntrinsicAttributes &
+    JSX.IntrinsicClassAttributes<StatusBar> &
+    Readonly<StatusBarProps> &
+    Readonly<{children?: React.ReactNode}>,
+) => {
+  const isFocused = useIsFocused();
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
+};
+
+export default FocusedStatusBar;
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,21 @@
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import FocusedStatusBar from '../../components/FocusedStatusBar';
+import {COLORS} from '../../constants';
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FocusedStatusBar backgroundColor={COLORS.primary} />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default Home;
